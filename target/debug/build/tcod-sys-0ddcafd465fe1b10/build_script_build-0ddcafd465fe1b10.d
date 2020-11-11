506fb062549c8d2fd1d133b596dcc4a0780c6eef
Gink3 <telltaylor13@gmail.com>
2020-11-11 14:14:28 -0600

    Removed target data

--- target/debug/build/tcod-sys-0ddcafd465fe1b10/build_script_build-0ddcafd465fe1b10.d
+++
@@ -1,7 +0,0 @@
-/Users/taylorking/Documents/GitHub/Rend/target/debug/build/tcod-sys-0ddcafd465fe1b10/build_script_build-0ddcafd465fe1b10: /Users/taylorking/.cargo/registry/src/github.com-1ecc6299db9ec823/tcod-sys-5.0.1/build.rs
-
-/Users/taylorking/Documents/GitHub/Rend/target/debug/build/tcod-sys-0ddcafd465fe1b10/build_script_build-0ddcafd465fe1b10.d: /Users/taylorking/.cargo/registry/src/github.com-1ecc6299db9ec823/tcod-sys-5.0.1/build.rs
-
-/Users/taylorking/.cargo/registry/src/github.com-1ecc6299db9ec823/tcod-sys-5.0.1/build.rs:
-
-# env-dep:CRATER_TASK_TYPE
--- target/debug/deps/pkg_config-bca52c1887980fa2.d
+++
@@ -1,7 +0,0 @@
-/Users/taylorking/Documents/GitHub/Rend/target/debug/deps/pkg_config-bca52c1887980fa2.rmeta: /Users/taylorking/.cargo/registry/src/github.com-1ecc6299db9ec823/pkg-config-0.3.19/src/lib.rs
-
-/Users/taylorking/Documents/GitHub/Rend/target/debug/deps/libpkg_config-bca52c1887980fa2.rlib: /Users/taylorking/.cargo/registry/src/github.com-1ecc6299db9ec823/pkg-config-0.3.19/src/lib.rs
-
-/Users/taylorking/Documents/GitHub/Rend/target/debug/deps/pkg_config-bca52c1887980fa2.d: /Users/taylorking/.cargo/registry/src/github.com-1ecc6299db9ec823/pkg-config-0.3.19/src/lib.rs
-
-/Users/taylorking/.cargo/registry/src/github.com-1ecc6299db9ec823/pkg-config-0.3.19/src/lib.rs:
